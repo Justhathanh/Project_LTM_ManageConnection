a261534a80b6e66d5a9c657d1e8f3d16cd08418e
Justhathanh <hvthanh1125@gmail.com>
2025-08-25 02:07:24 +0700

    them folder cua gateway

---
+++ WifiGuard/server/src/main/java/com/wifiguard/server/gateway/RouterGateway.java
@@ -0,0 +1,7 @@
+package com.wifiguard.server.gateway;
+import java.util.Map;
+public interface RouterGateway {
+  Map<String,Device> getConnectedDevices() throws Exception;
+  default void block(String mac,int banSeconds) throws Exception {}
+  record Device(String mac,int rssi,int txRateMbps){}
+}
